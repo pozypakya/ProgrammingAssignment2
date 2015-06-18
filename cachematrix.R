3f8268ef9589974876d875232e7a66db888c1ff3
akunyer <akunyer@gmail.com>
2015-06-19 00:45:47 +0800

    latest commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,12 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 ## By Fauzy Che Yayah
-## Write a short comment describing this function
+
+# This function will create list containing function for
+# a) setter the matrix value
+# b) getter the matrix value
+# c) setter the inverse matrix value
+# d) getter the inverse matrix value
 
 makeCacheMatrix <- function(x = matrix()) {
 inv <- NULL
@@ -15,11 +20,11 @@ inv <- NULL
     list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
-
 ## Write a short comment describing this function
+# This function have the return value of the inverse matrix.
+# 1st chech the if the inverse matrix is computed or not before , if yes then return the result and skip. If no then it will compute the inverse , setting the value into cache using the setinverse funtion
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
 		 inv <- x$getinverse()
     if(!is.null(inv)) {
         message("getting cached data.")
@@ -30,3 +35,17 @@ cacheSolve <- function(x, ...) {
     x$setinverse(inv)
     inv
 }
+
+# Sample run
+#--------------------------------------------
+# > x = rbind(c(1, -1/2), c(-1/2, 1))
+# > m = makeCacheMatrix(x)
+# > m$get()
+     # [,1] [,2]
+# [1,]  1.0 -0.5
+# [2,] -0.5  1.0
+# > cacheSolve(m)
+          # [,1]      [,2]
+# [1,] 1.3333333 0.6666667
+# [2,] 0.6666667 1.3333333
+#
